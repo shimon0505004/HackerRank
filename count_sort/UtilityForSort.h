fe64ded07f09356db8ef24ccd5a01b92bbb323ba
SHAIKH SHAWON AREFIN SHIMON <shimon0505004@gmail.com>
2016-10-02 02:30:39 -0700

    Update UtilityForSort.h

--- count_sort/UtilityForSort.h
+++ count_sort/UtilityForSort.h
@@ -1,9 +1,13 @@
 #pragma once
 #include<vector>
+#include<list>
 
 namespace UtilityForSort 
 {
-	std::vector<int> count_sort(std::vector<int> inputArray);
+	std::vector<int> count_sort(const std::vector<int>& inputArray);
 	int max_element_index(const std::vector<int>& inputArray);
 	int min_element_index(const std::vector<int>& inputArray);
+
+	std::vector<int> merge_sort(std::vector<int> inputArray);
+	std::vector<int> merge(std::vector<int> array1, std::vector<int> array2);
 };
